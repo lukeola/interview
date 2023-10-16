db36b6a4798ac874c9543de121e261fed49e5929
Luke Olawale <lukeolawale4@gmail.com>
2023-10-16 18:42:35 +0100

    Update Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Posts from "./pages/posts/Posts";
+import Home from "./pages/home/Home";
+import Users from "./pages/users/Users";
+import Comments from "./pages/comments/Comments";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/comments" element={<Comments />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

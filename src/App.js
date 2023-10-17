61cc6f615389cb076af38bedb44d70a4231bc88b
Luke Olawale <lukeolawale4@gmail.com>
2023-10-17 02:17:06 +0100

    update routes

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import Posts from "./pages/posts/Posts";
 import Home from "./pages/home/Home";
 import Users from "./pages/users/Users";
 import Comments from "./pages/comments/Comments";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <Router>
       <div>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/posts" element={<Posts />} />
           <Route path="/users" element={<Users />} />
           <Route path="/comments" element={<Comments />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
